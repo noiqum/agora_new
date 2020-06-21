15438d6b6bdae81fdd34a3685f1cc2bf907ff7b7
unknown <onurcoskun99@gmail.com>
2020-06-21 18:23:43 +0300

    logout navlink redirect to homepage

--- src/components/navbar/navLinks.jsx
+++ src/components/navbar/navLinks.jsx
@@ -1,64 +1,110 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux'
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
- function navLinks({username,signup,login,loginClicked,signupClicked,logoutClicked,id,onDefaultClick,profilePhoto}) {
+function navLinks({
+  username,
+  signup,
+  login,
+  loginClicked,
+  signupClicked,
+  logoutClicked,
+  id,
+  onDefaultClick,
+  profilePhoto,
+}) {
+  const linkStyle = {
+    all: "inherit",
+  };
 
-    const linkStyle ={
-        
-        'all':'inherit'
-    };
-   
-    const profilePhotoHolder=()=>{
-           return profilePhoto !== '' ? profilePhoto.downloadUrl : 
-               'https://firebasestorage.googleapis.com/v0/b/agora-event-platform.appspot.com/o/userPlaceHolder.png?alt=media&token=9b6cc96e-a380-4d30-9060-72cc0294ce89'
-           
-    }
+  const profilePhotoHolder = () => {
+    return profilePhoto !== ""
+      ? profilePhoto.downloadUrl
+      : "https://firebasestorage.googleapis.com/v0/b/agora-event-platform.appspot.com/o/userPlaceHolder.png?alt=media&token=9b6cc96e-a380-4d30-9060-72cc0294ce89";
+  };
 
+  return (
+    <div>
+      <nav className="navbar__nav">
+        <ul className="navbar__nav__list">
+          {login && (
+            <Link
+              style={linkStyle}
+              className="link-router"
+              to="/event"
+              onClick={onDefaultClick}
+            >
+              <li className="navbar__nav__list__link">event</li>
+            </Link>
+          )}
 
-    return (
-        <div>
-        <nav className='navbar__nav'>
-                <ul className="navbar__nav__list">
-                    {login &&
-                    <Link  style={linkStyle} className='link-router' to='/event' onClick={onDefaultClick}> 
-                    <li className="navbar__nav__list__link">event</li>
-                    </Link>}
-                   
-                    {login &&
-                    <Link style={linkStyle} to='/people' onClick={onDefaultClick}>
-                    <li className="navbar__nav__list__link">people</li>
-                    </Link> }
+          {login && (
+            <Link style={linkStyle} to="/people" onClick={onDefaultClick}>
+              <li className="navbar__nav__list__link">people</li>
+            </Link>
+          )}
 
-                    {!login && <Link style={linkStyle} to='/login'> <li onClick={loginClicked} className="navbar__nav__list__link">log in</li></Link>}
-                    { login && <li onClick={logoutClicked} className="navbar__nav__list__link">log out</li>}
-                    {!login && !signup && <Link style={linkStyle} to='/signup'> <li onClick={signupClicked} className="navbar__nav__list__link">sign up</li></Link>} 
-                    
-                    {login && <Link style={linkStyle} 
-                    to={{pathname:`/profile/:${id}`}}
-                    onClick={onDefaultClick}>
-                    <li className="navbar__nav__list__link">
-                    {username}</li></Link>}
+          {!login && (
+            <Link style={linkStyle} to="/login">
+              {" "}
+              <li onClick={loginClicked} className="navbar__nav__list__link">
+                log in
+              </li>
+            </Link>
+          )}
+          {login && (
+            <Link style={linkStyle} to="/" exact>
+              {" "}
+              <li
+                onClick={logoutClicked}
+                to="/"
+                className="navbar__nav__list__link"
+              >
+                log out
+              </li>
+            </Link>
+          )}
+          {!login && !signup && (
+            <Link style={linkStyle} to="/signup">
+              {" "}
+              <li onClick={signupClicked} className="navbar__nav__list__link">
+                sign up
+              </li>
+            </Link>
+          )}
 
-                    {login && <Link style={linkStyle} 
-                    to={{pathname:`/profile/:${id}`}}
-                    onClick={onDefaultClick}>
-                    <li className="navbar__nav__list__link">
-                     <img  alt='profile_pic'src={`${profilePhotoHolder()}`}></img>
-                    </li></Link>}
+          {login && (
+            <Link
+              style={linkStyle}
+              to={{ pathname: `/profile/:${id}` }}
+              onClick={onDefaultClick}
+            >
+              <li className="navbar__nav__list__link">{username}</li>
+            </Link>
+          )}
 
-                </ul>
-            </nav>
-        </div>
-    )
-
+          {login && (
+            <Link
+              style={linkStyle}
+              to={{ pathname: `/profile/:${id}` }}
+              onClick={onDefaultClick}
+            >
+              <li className="navbar__nav__list__link">
+                <img alt="profile_pic" src={`${profilePhotoHolder()}`}></img>
+              </li>
+            </Link>
+          )}
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-const mapStateToProps=state=>{
-    return{
-        id:state.auth.user.id,
-        profilePhoto:state.auth.user.profilePhoto
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    id: state.auth.user.id,
+    profilePhoto: state.auth.user.profilePhoto,
+  };
+};
 
-export default connect(mapStateToProps)(navLinks)
+export default connect(mapStateToProps)(navLinks);
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,105 +1,127 @@
-import React, { Component } from 'react';
-import Navlink from './navLinks';
-import gsap from 'gsap';
-import {connect} from 'react-redux';
-import { loginModal , logout } from '../../store/actions/authActions';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import Navlink from "./navLinks";
+import gsap from "gsap";
+import { connect } from "react-redux";
+import { loginModal, logout } from "../../store/actions/authActions";
+import { Link } from "react-router-dom";
 
-
-
-const linkStyle ={
-    textDecoration:'none',
-   
+const linkStyle = {
+  textDecoration: "none",
 };
 
 // let screenwidth =window.innerWidth;
 
 export class navbar extends Component {
-
-    constructor(props){
-        super(props)
-        this.state={
-            burgerClicked:false,
-            login:this.props.loginState,
-            signup:this.props.signupState,
-            currentUser:null,
-            displayname:null
-        }
-    }
-
-
-    handleBurger=()=>{
-        if(this.state.burgerClicked===false){
-        gsap.to('.navbar__nav',{duration:1.5,y:450,ease:'power2.out'})
-        this.setState({burgerClicked:true})
-        gsap.to('.navbar__burger-2',{
-            opacity:0,duration:1,x:-10,ease:'power2.out'}
-        )
-        gsap.to('.navbar__burger-1',{duration:1,ease:'power2.out',rotate:45,transformOrigin:'20% 50% 7px'})
-        gsap.to('.navbar__burger-3',{duration:1,ease:'power2.out',rotate:-45,transformOrigin:'20% 50% 7px'})
-        }
-        if(this.state.burgerClicked===true){
-            gsap.to('.navbar__nav',{duration:1.5,y:0,ease:'power2.out'})
-            this.setState({burgerClicked:false})
-            gsap.to('.navbar__burger-2',{
-                opacity:1,duration:1,x:0,ease:'power2.out'}
-            )
-            gsap.to('.navbar__burger-1',{duration:1,ease:'power2.out',rotate:0,translate:-7})
-            gsap.to('.navbar__burger-3',{duration:1,ease:'power2.out',rotate:0,translate:7})
-        }
-    }
-
-    handleLogin=()=>{
-        this.props.onLoginClick();
-        this.setState({login:this.props.loginState});
-        this.handleBar();
-    }
-
-    handleLogout=()=>{
-        this.props.onLogoutClick();
-        this.handleBar();
-    }
-   
-    handleBar=()=>{
-        let screenwidth =window.innerWidth;
-        if(screenwidth <600){
-            this.handleBurger();
-        }
-    }
-    render() {
-        return (
-        <div className='navbar'>
-            <Link style={linkStyle} to='/'><div className="navbar__logo">agora</div></Link>
-            <div className="navbar__burger-1" onClick={this.handleBurger}></div>
-            <div className="navbar__burger-2" onClick={this.handleBurger}></div>
-            <div className="navbar__burger-3" onClick={this.handleBurger}></div>
-            <Navlink 
-             loginClicked={this.handleLogin} 
-             signupClicked ={this.handleBar} 
-             login={this.props.loginState} 
-             signup={this.state.signup}
-             logoutClicked={this.handleLogout}
-             username={this.props.name}
-             onDefaultClick={this.handleBar}
-             />
-            
-        </div>
-        )
-    }
-}
-
-const mapStateToProps=(state)=>{
-   return  {
-       loginState:state.auth.login,
-       signupState:state.auth.signup,
-       name:state.auth.user.displayName
+  constructor(props) {
+    super(props);
+    this.state = {
+      burgerClicked: false,
+      login: this.props.loginState,
+      signup: this.props.signupState,
+      currentUser: null,
+      displayname: null,
     };
-}
-const mapDispatchToProps=dispatch=>{
-    return {
-        onLoginClick:()=>dispatch(loginModal()),
-        onLogoutClick:()=>dispatch(logout())
+  }
+
+  handleBurger = () => {
+    if (this.state.burgerClicked === false) {
+      gsap.to(".navbar__nav", { duration: 1.5, y: 450, ease: "power2.out" });
+      this.setState({ burgerClicked: true });
+      gsap.to(".navbar__burger-2", {
+        opacity: 0,
+        duration: 1,
+        x: -10,
+        ease: "power2.out",
+      });
+      gsap.to(".navbar__burger-1", {
+        duration: 1,
+        ease: "power2.out",
+        rotate: 45,
+        transformOrigin: "20% 50% 7px",
+      });
+      gsap.to(".navbar__burger-3", {
+        duration: 1,
+        ease: "power2.out",
+        rotate: -45,
+        transformOrigin: "20% 50% 7px",
+      });
     }
+    if (this.state.burgerClicked === true) {
+      gsap.to(".navbar__nav", { duration: 1.5, y: 0, ease: "power2.out" });
+      this.setState({ burgerClicked: false });
+      gsap.to(".navbar__burger-2", {
+        opacity: 1,
+        duration: 1,
+        x: 0,
+        ease: "power2.out",
+      });
+      gsap.to(".navbar__burger-1", {
+        duration: 1,
+        ease: "power2.out",
+        rotate: 0,
+        translate: -7,
+      });
+      gsap.to(".navbar__burger-3", {
+        duration: 1,
+        ease: "power2.out",
+        rotate: 0,
+        translate: 7,
+      });
+    }
+  };
+
+  handleLogin = () => {
+    this.props.onLoginClick();
+    this.setState({ login: this.props.loginState });
+    this.handleBar();
+  };
+
+  handleLogout = () => {
+    this.props.onLogoutClick();
+    this.handleBar();
+  };
+
+  handleBar = () => {
+    let screenwidth = window.innerWidth;
+    if (screenwidth < 600) {
+      this.handleBurger();
+    }
+  };
+  render() {
+    return (
+      <div className="navbar">
+        <Link style={linkStyle} to="/">
+          <div className="navbar__logo">agora</div>
+        </Link>
+        <div className="navbar__burger-1" onClick={this.handleBurger}></div>
+        <div className="navbar__burger-2" onClick={this.handleBurger}></div>
+        <div className="navbar__burger-3" onClick={this.handleBurger}></div>
+        <Navlink
+          loginClicked={this.handleLogin}
+          signupClicked={this.handleBar}
+          login={this.props.loginState}
+          signup={this.state.signup}
+          logoutClicked={this.handleLogout}
+          username={this.props.name}
+          onDefaultClick={this.handleBar}
+        />
+      </div>
+    );
+  }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps) (navbar);
+const mapStateToProps = (state) => {
+  return {
+    loginState: state.auth.login,
+    signupState: state.auth.signup,
+    name: state.auth.user.displayName,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onLoginClick: () => dispatch(loginModal()),
+    onLogoutClick: () => dispatch(logout()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(navbar);
